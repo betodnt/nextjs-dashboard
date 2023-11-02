2effa9cf8debde0fa0bf6bcc95d26f487012ec9a
betodnt <dentgm@gmail.com>
2023-11-02 10:27:02 -0300

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: '%s | Acme Dashboard',
+  title: 'Acme Dashboard',
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
